9f182c91eb46b1f120b6dd308bb3b2557c05cb00
mahmud-bhuiyan <bh.mahmud@gmail.com>
2023-12-21 23:55:17 +0600

    navbar updated

--- src/Components/common/Navbar.jsx
+++ src/Components/common/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Link, useNavigate } from "react-router-dom";
 import { demo_user } from "../../assets/images";
-import { useContext } from "react";
+import { useContext, useState } from "react"; // Import useState
 import { UserContext } from "../../Context/UserContext";
 import { logoutUser } from "../../services/api/User";
 import { toast } from "react-toastify";
@@ -12,15 +12,26 @@ const Navbar = () => {
 
   const navigate = useNavigate();
 
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false); // Add state to track dropdown open/close
+
   const handleLogout = async () => {
     try {
       await logoutUser();
       window.location.reload();
+      setIsDropdownOpen(false); // Close the dropdown
       navigate("/users/login");
     } catch (error) {
       console.error("Logout failed:", error);
       toast.error(error.message || "Logout failed");
     }
+  };
+
+  const closeDropdown = () => {
+    setIsDropdownOpen(false);
+  };
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen((prev) => !prev);
   };
 
   return (
@@ -38,22 +49,27 @@ const Navbar = () => {
             tabIndex={0}
             role="button"
             className="btn btn-sm btn-ghost btn-circle border-2 border-slate-400 avatar"
+            onClick={toggleDropdown} 
           >
             <div className="w-6 rounded-full">
               <img src={demo_user} alt="demo_user" />
             </div>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <Link to="/users/profile">Profile</Link>
-            </li>
-            <li>
-              <button onClick={handleLogout}>Logout</button>
-            </li>
-          </ul>
+          {isDropdownOpen && ( 
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <Link to="/users/profile" onClick={closeDropdown}>
+                  Profile
+                </Link>
+              </li>
+              <li>
+                <button onClick={handleLogout}>Logout</button>
+              </li>
+            </ul>
+          )}
         </div>
       </div>
     </div>
